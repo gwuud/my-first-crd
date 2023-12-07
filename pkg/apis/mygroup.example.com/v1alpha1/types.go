3e9444f9a3f3f569dc97761ed74604781cbde811
Gareth Wood <gareth.wuud@icloud.com>
2023-12-07 15:00:37 +0000

    deepcopy-gen and client-go generator files

--- pkg/apis/mygroup.example.com/v1alpha1/types.go
+++ pkg/apis/mygroup.example.com/v1alpha1/types.go
@@ -3,6 +3,7 @@ package v1alpha1
 import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +genclient
 type MyResource struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -11,12 +12,14 @@ type MyResource struct {
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +genclient
 type MyResourceSpec struct {
 	Image   string `json:"image"`
 	Species string `json:"species"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +genclient
 type MyResourceList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
